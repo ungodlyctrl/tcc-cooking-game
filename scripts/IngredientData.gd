69f4a85b3fe5dfd8bbc3197d11f69102c5a5b783
liperrr <silva.sfelipe19@gmail.com>
2025-09-01 00:53:30 -0300

    31/08

---
+++ scripts/IngredientData.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name IngredientData
+
+@export var id: String
+@export var display_name: String
+@export var container: Texture2D
+@export var initial_state: String = "raw"
+@export var states: Dictionary[String, Texture2D] = {} 
+## Exemplo: { "raw": Texture2D, "cut": Texture2D, "cooked": Texture2D }
